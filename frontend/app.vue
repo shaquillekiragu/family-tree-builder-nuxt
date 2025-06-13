e3f22f2d542c3d5893459e4c06a5075747499d0c
Shaquille Kiragu <shaquille@toru.digital>
2025-06-13 17:14:40 +0100

    Create initial pages

--- frontend/app.vue
+++ frontend/app.vue
@@ -1,6 +1,11 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+  <div class="w-screen h-screen flex flex-col">
+    <div class="h-1/2">
+      <NuxtRouteAnnouncer />
+      <NuxtWelcome />
+    </div>
+    <div class="h-1/2">
+      <h1>HELLO</h1>
+    </div>
   </div>
 </template>

e366b4073e5f03cacaa8e14f968a1679fca6e92c
Shaquille Kiragu <shaquille@toru.digital>
2025-06-17 14:29:55 +0100

    Create PersonCard.vue component

---
+++ components/PersonCard.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <div>
+      <NuxtImg></NuxtImg>
+    </div>
+    <div>
+      <p>{{}}</p>
+    </div>
+    <div>
+      <p>Born: {{}}</p>
+    </div>
+  </div>
+</template>

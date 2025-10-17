17c62994b75499896c3f1752b3730a9af82a406b
Shaquille Kiragu <shaquille@toru.digital>
2025-10-17 17:19:37 +0100

    replace and builld components

---
+++ components/generation/component.vue
@@ -0,0 +1,20 @@
+<template>
+	<section
+		v-for="person in generation"
+		class="w-full p-10 flex justify-center items-center gap-10"
+	>
+		<PersonComponent :person="person" />
+	</section>
+</template>
+
+<script setup lang="ts">
+import type { PropType } from 'vue';
+import type IPerson from '~/types/person';
+
+const { generation } = defineProps({
+	generation: {
+		type: Array as PropType<IPerson[]>,
+		required: true,
+	},
+});
+</script>
---
+++ components/person/component.vue
@@ -0,0 +1,18 @@
+<template>
+	<article class="w-50 h-75 border flex flex-col gap-2 p-2">
+		<h4 class="">Name: {{ person.full_name }}</h4>
+		<p class="">Year of Birth: {{ person.year_of_birth.toString() }}</p>
+	</article>
+</template>
+
+<script setup lang="ts">
+import type { PropType } from 'vue';
+import type { IPerson } from '~/types/person';
+
+const { person } = defineProps({
+	person: {
+		type: Object as PropType<IPerson>,
+		required: true,
+	},
+});
+</script>
